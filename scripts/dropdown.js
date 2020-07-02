87d3a92251d96a7af60feb41170b70326cdb902c
spacemandev <devbharel@gmail.com>
2020-07-01 22:49:07 -0500

    got X to show up and cancel the bar properly

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -4,7 +4,7 @@ export async function generateBar() {
   log("Generating Bar");
   let oldBar = document.getElementById("token-dropdown-bar");
   if (oldBar) {
-    oldbar.remove();
+    oldBar.remove();
   }
 
   $(document.body).off("click.showTokenActionBar");
@@ -14,17 +14,18 @@ export async function generateBar() {
     $dispOptions.remove();
     $(document.body).off("click.showTokenActionBar");
   };
-  //no need to check if tokens > 1, because Hook is only called when token is selected
-  let selectedActor = canvas.tokens.controlled[0].actor;
+
   let display, data, targetId;
-  if (selectedActor == null) {
+  try {
+    let selectedActor = canvas.tokens.controlled[0].actor;
+    display = "flex";
+    data = getContent(selectedActor); //getData(selectedActor);
+    targetId = selectedActor._id;
+  } catch (e) {
+    //selected actor not defined
     display = "none";
     data = "";
     targetId = "";
-  } else {
-    display = "flex";
-    data = "<p>Test</p>"; //getData(selectedActor);
-    targetId = selectedActor._id;
   }
 
   //save coords and retreive => client/user flag (TokenBar, Coord)
@@ -35,8 +36,7 @@ export async function generateBar() {
     .getBoundingClientRect();
   let x = (controlBounds = controlBounds.right + 50);
 
-  log(data);
-  $(
+  const $dispOptions = $(
     `<div class="tokenbar" targetID="${targetId}" id="token-dropdown-bar" style="display: ${display}; z-index: 70; position: fixed; top: ${y}px; height: auto; left: ${x}px; background-color: #bbb">${data}</div>`
   ).appendTo(document.body);
 
@@ -61,4 +61,19 @@ export async function generateBar() {
       return;
     }
   });
+}
+
+function getContent(actor) {
+  let template = `
+    <div id="actionDialog">
+      <div class="show-action-form">
+        <div class="show-action-dropdowns">
+          <div class="show-action-dropdown">
+            <button value="showActionClose" class="show-action-dropdown-button">[X]</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  return template;
 }
