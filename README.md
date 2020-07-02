e4f32871cd35d434b507d622e513ca3afebf0dfe
spacemandev <devbharel@gmail.com>
2020-07-02 02:04:48 -0500

    1.0 release

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Token Bar (SWADE)
 
 This is a token bar inspired by the 5E Tokenbar module written by Kekilla0 (https://github.com/Kekilla0/TokenBar)
+
+Creates a bar when a token is clicked on that lets you do 1 Click rolls for Attributes & Skills, spend Bennies, toggle Statuses, Increase/Decrease Wounds & Fatigue
--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -31,18 +31,15 @@ export async function generateBar() {
   //save coords and retreive => client/user flag (TokenBar, Coord)
   let navBounds = document.getElementById("navigation").getBoundingClientRect();
   let y = navBounds.bottom + 20;
-  let controlBounds = document
-    .getElementById("controls")
-    .getBoundingClientRect();
+  let controlBounds = document.getElementById("controls").getBoundingClientRect();
   let x = (controlBounds = controlBounds.right + 50);
 
   const $dispOptions = $(
     `<div class="tokenbar" targetID="${targetId}" id="token-dropdown-bar" style="display: ${display}; z-index: 70; position: fixed; top: ${y}px; height: auto; left: ${x}px; background-color: #bbb">${data}</div>`
   ).appendTo(document.body);
 
-  $(document.body).on("click.showTokenActionBar", (evt) => {
+  $(document.body).on("click.showTokenActionBar", async (evt) => {
     if (evt.target.value == undefined) return;
-    log(evt);
     Hooks.once(`updateActor`, (updatedActor) => {
       // If the selected actor is updated (new item, skill, whatever) regenerate the bar
       if (updatedActor.data._id == selectedActor.data._id) {
@@ -60,20 +57,175 @@ export async function generateBar() {
       cancel();
       return;
     }
+
+    await handleEvent(evt, canvas.tokens.controlled[0].actor);
+  });
+}
+
+async function handleEvent(evt, actor) {
+  if (evt.target.value == "" || evt.target.value == undefined || evt.target.value.split(".").length != 2) {
+    return;
+  }
+
+  let type = evt.target.value.split(".")[0];
+  let value = evt.target.value.split(".")[1];
+  if (type == "attributes") {
+    let rollString = "";
+    if (actor.data.data.wildcard) {
+      rollString = `{1d${actor.data.data.attributes[value].die.sides}x=, 1d${
+        actor.data.data.attributes[value]["wild-die"].sides
+      }x=}kh +${actor.calcWoundFatigePenalties()} +${actor.calcStatusPenalties()}`;
+    } else {
+      rollString = `1d${
+        actor.data.data.attributes[value].die.sides
+      }x= +${actor.calcWoundFatigePenalties()} +${actor.calcStatusPenalties()}`;
+    }
+    roll(actor, `${value.toUpperCase()} Roll`, rollString);
+  } else if (type == "skill") {
+    let skill = actor.items.find((el) => el.data.name == value);
+    let rollString = "";
+    if (actor.data.data.wildcard) {
+      rollString = `{1d${skill.data.data.die.sides}x=, 1d${
+        skill.data.data["wild-die"].sides
+      }x=}kh +${actor.calcWoundFatigePenalties()} +${actor.calcStatusPenalties()}`;
+    } else {
+      rollString = `1d${
+        skill.data.data.die.sides
+      }x= +${actor.calcWoundFatigePenalties()} +${actor.calcStatusPenalties()}`;
+    }
+    roll(actor, `${value.toUpperCase()} Roll`, rollString);
+  } else if (type == "status") {
+    if (value == "shaken") {
+      await actor.update({ "data.status.isShaken": !actor.data.data.status.isShaken });
+    } else if (value == "distracted") {
+      await actor.update({ "data.status.isDistracted": !actor.data.data.status.isDistracted });
+    } else if (value == "vulnerable") {
+      await actor.update({ "data.status.isVulnerable": !actor.data.data.status.isVulnerable });
+    }
+  } else if (type == "wounds") {
+    if (value == "increase") {
+      if (actor.data.data.wounds.value + 1 > actor.data.data.wounds.max) {
+        ui.notifications.warn("Wounds at Max! Character is Incapaciated");
+      } else {
+        await actor.update({ "data.wounds.value": actor.data.data.wounds.value + 1 });
+      }
+    } else if (value == "decrease") {
+      if (actor.data.data.wounds.value - 1 < actor.data.data.wounds.min) {
+        ui.notifications.warn("Character has no wounds currently!");
+      } else {
+        await actor.update({ "data.wounds.value": actor.data.data.wounds.value - 1 });
+      }
+    }
+  } else if (type == "fatigue") {
+    if (value == "increase") {
+      if (actor.data.data.fatigue.value + 1 > actor.data.data.fatigue.max) {
+        ui.notifications.warn("Fatigue at Max! Character is Incapaciated");
+      } else {
+        await actor.update({ "data.fatigue.value": actor.data.data.fatigue.value + 1 });
+      }
+    } else if (value == "decrease") {
+      if (actor.data.data.fatigue.value - 1 < actor.data.data.fatigue.min) {
+        ui.notifications.warn("Character has no fatigue currently!");
+      } else {
+        await actor.update({ "data.fatigue.value": actor.data.data.fatigue.value - 1 });
+      }
+    }
+  } else if (type == "benny") {
+    if (value == "spend") {
+      actor.spendBenny();
+    }
+  }
+}
+
+function roll(selected, rollDescription, rollString) {
+  ChatMessage.create({
+    speaker: {
+      actor: selected,
+      alias: `${selected.name} | ${rollDescription}`,
+    },
+    roll: new Roll(rollString).roll(),
+    type: CONST.CHAT_MESSAGE_TYPES.ROLL,
   });
 }
 
 function getContent(actor) {
   let template = `
-    <div id="actionDialog">
-      <div class="show-action-form">
-        <div class="show-action-dropdowns">
-          <div class="show-action-dropdown">
-            <button value="showActionClose" class="show-action-dropdown-button">[X]</button>
-          </div>
+    <div class="dropdown-list">
+      ${getAttributesDropdown()}
+      ${getSkillsDropdown(actor)}
+      <div class="dropdown-header">
+        <button value="showStatusToggle">Status</button>
+        <div class="dropdown-content">
+          <button value="status.shaken">Toggle ${game.i18n.localize("SWADE.Shaken")}</button>
+          <button value="status.distracted">Toggle ${game.i18n.localize("SWADE.Distr")}</button>
+          <button value="status.vulnerable">Toggle ${game.i18n.localize("SWADE.Vuln")}</button>
         </div>
+      </div>
+      <div class="dropdown-header">
+        <button value="showWounds">${game.i18n.localize("SWADE.Wounds")}</button>
+        <div class="dropdown-content">
+          <button value="wounds.increase">+1</button>
+          <button value="wounds.decrease">-1</button>
+        </div>        
+      </div>
+      <div class="dropdown-header">
+        <button value="showFatigue">${game.i18n.localize("SWADE.Fatigue")}</button>
+        <div class="dropdown-content">
+          <button value="fatigue.increase">+1</button>
+          <button value="fatigue.decrease">-1</button>
+        </div>        
+      </div>
+      ${ifWCSpendBenny(actor)}
+      <div class="dropdown-header">
+        <button value="showActionClose">[X]</button>
       </div>
     </div>
   `;
   return template;
+}
+
+function getAttributesDropdown() {
+  let template = `
+    <div class="dropdown-header">
+      <button value="showAttributes">${game.i18n.localize("SWADE.Attributes")}</button>
+      <div class="dropdown-content">
+        <button value="attributes.agility">${game.i18n.localize("SWADE.AttrAgi")}</button>
+        <button value="attributes.smarts">${game.i18n.localize("SWADE.AttrSma")}</button>
+        <button value="attributes.spirit">${game.i18n.localize("SWADE.AttrSpr")}</button>
+        <button value="attributes.strength">${game.i18n.localize("SWADE.AttrStr")}</button>
+        <button value="attributes.vigor">${game.i18n.localize("SWADE.AttrVig")}</button>
+      </div>
+    </div>
+  `;
+  return template;
+}
+
+function getSkillsDropdown(actor) {
+  let skillsList = actor.items.filter((el) => el.data.type == "skill");
+  let skillButtons = ``;
+  for (let i = 0; i < skillsList.length; i++) {
+    skillButtons += `<button value="skill.${skillsList[i].data.name}">${skillsList[i].data.name}</button>`;
+  }
+
+  let template = `
+  <div class="dropdown-header">
+    <button value="showSkills">${game.i18n.localize("SWADE.Skills")}</button>
+    <div class="dropdown-content">
+    ${skillButtons}
+    </div>
+  </div>
+  `;
+  return template;
+}
+
+function ifWCSpendBenny(actor) {
+  if (actor.data.data.wildcard) {
+    return `
+    <div class="dropdown-header">
+      <button value="benny.spend">Spend Benny</button>
+    </div>
+    `;
+  } else {
+    return "";
+  }
 }
